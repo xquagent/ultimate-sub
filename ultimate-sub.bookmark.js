0aefaf518c4240e7b2376c9bc99e698d0489db4a
xquagent <xquagent1000@gmail.com>
2026-08-11 16:43:04 -0500

    Update ultimate-sub.bookmark.js

--- ultimate-sub.bookmark.js
+++ ultimate-sub.bookmark.js
@@ -1,1 +1,1 @@
-javascript:(function(){let s=document.createElement("script");s.src="http://127.0.0.1:8787/ultimate-sub.js?v="+Date.now();s.onload=()=>s.remove();s.onerror=()=>alert("Ultimate Sub could not load from the local server.");document.head.appendChild(s);})();
+javascript:(function(){let s=document.createElement("script");s.src="https://xquagent.github.io/ultimate-sub/ultimate-sub.js?v="+Date.now();s.onload=()=>s.remove();s.onerror=()=>alert("Ultimate Sub could not load from GitHub Pages.");document.head.appendChild(s);})();
